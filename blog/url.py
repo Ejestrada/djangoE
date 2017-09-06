f9cb618eb4ac989b93b7048d79251b4da0389f68
Ejestrada <erick08054estrada@outlook.com>
2017-09-06 00:26:49 -0600

    nueva configuracion cambio de estilo creacion ce css

--- blog/url.py
+++ blog/url.py
@@ -2,5 +2,7 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-        url(r'^$', views.listar_pub),
-    ]
+url(r'^$', views.listar_pub),
+url(r'^postea/(?P<pk>[0-9]+)/$', views.detalle_pub,name='postea'),
+
+]
