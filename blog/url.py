0db646810eb19d29f8f8776790b8b48756589ef8
Ejestrada <erick08054estrada@outlook.com>
2017-09-19 12:18:00 -0600

    editado de formulario

--- blog/url.py
+++ blog/url.py
@@ -5,4 +5,7 @@ urlpatterns = [
 url(r'^$', views.listar_pub),
 url(r'^postea/(?P<pk>[0-9]+)/$', views.detalle_pub,name='postea'),
 url(r'^post/new/$', views.post_new, name='post_new'),
+  url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+
+
 ]
