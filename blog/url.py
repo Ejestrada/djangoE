1b12c27b9a73dca225a3c82494e9e90b59255ff3
Ejestrada <erick08054estrada@outlook.com>
2017-09-26 18:11:37 -0600

    modificacion de borrador

--- blog/url.py
+++ blog/url.py
@@ -2,23 +2,12 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
- url(r'^$', views.listar_pub,name='listar_pub'),
+    url(r'^$', views.listar_pub,name='listar_pub'),
     url(r'^postea/(?P<pk>[0-9]+)/$', views.detalle_pub,name='postea'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
     url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
     url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
-=======
-url(r'^$', views.listar_pub,name='listar_pub' ),
-url(r'^postea/(?P<pk>[0-9]+)/$', views.detalle_pub,name='detalle_pub'),
-url(r'^post/new/$', views.post_new, name='post_new'),
-url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
-url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
-url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
-url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
->>>>>>> 54824ed86cea78e288af450baaf3fa34baba44e0
-
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Publicacion
 from .forms import PostForm
-from django.shortcuts import redirect
 
 def listar_pub(request):
     pub=Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
@@ -14,7 +13,6 @@ def detalle_pub(request, pk):
     return render(request,'blog/detalle_publicacion.html',{'p':p})
 
 def post_new(request):
-<<<<<<< HEAD
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
@@ -39,32 +37,6 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
-=======
-        if request.method == "POST":
-            form = PostForm(request.POST)
-            if form.is_valid():
-                post = form.save(commit=False)
-                post.autor = request.user
-                #post.fecha_publicacion = timezone.now()
-                post.save()
-                return redirect('detalle_pub', pk=post.pk)
-        else:
-            form = PostForm()
-        return render(request, 'blog/post_edit.html', {'form': form})
-
-def post_edit(request, pk):
-        post = get_object_or_404(Publicacion, pk=pk)
-        if request.method == "POST":
-            form = PostForm(request.POST, instance=post)
-            if form.is_valid():
-                post = form.save(commit=False)
-                post.author = request.user
-                post.save()
-                return redirect('detalle_pub', pk=post.pk)
-        else:
-            form = PostForm(instance=post)
-        return render(request, 'blog/post_edit.html', {'form': form})
->>>>>>> 54824ed86cea78e288af450baaf3fa34baba44e0
 
 def post_draft_list(request):
     posts = Publicacion.objects.filter(fecha_publicacion__isnull=True).order_by('fecha_creacion')
@@ -73,15 +45,7 @@ def post_draft_list(request):
 def post_publish(request, pk):
     post = get_object_or_404(Publicacion, pk=pk)
     post.publicar()
-<<<<<<< HEAD
     return redirect('postea', pk=pk)
-=======
-    return redirect('detalle_pub', pk=pk)
-
-def publish(self):
-    self.fecha_publicacion = timezone.now()
-    self.save()
->>>>>>> 54824ed86cea78e288af450baaf3fa34baba44e0
 
 def post_remove(request, pk):
     post = get_object_or_404(Publicacion, pk=pk)
